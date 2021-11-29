0084b0a9684ec90aeb9c6d713e15716f98cc1d23
dichioniccolo <dichioniccolo@gmail.com>
2021-11-29 12:56:17 +0100

    Fix lessthan test typo

--- src/index.ts
+++ src/index.ts
@@ -9,25 +9,29 @@ export function parse(
   return getParts(value).reduce((acc, part) => {
     // We want to give priority to the custom parsers
     for (const parser of Object.values(customParsers)) {
-      if (parser.isOfType(part)) {
-        const instance: Parser = new (<any>customParsers)[parser.name](part);
-
-        return {
-          ...acc,
-          ...instance.parse(acc),
-        };
+      if (!parser.isOfType(part)) {
+        continue;
       }
+
+      const instance: Parser = new (<any>customParsers)[parser.name](part);
+
+      return {
+        ...acc,
+        ...instance.parse(acc),
+      };
     }
 
     for (const parser of Object.values(defaultParsers)) {
-      if (parser.isOfType(part)) {
-        const instance = createInstance(parser.name, part);
-
-        return {
-          ...acc,
-          ...instance.parse(acc),
-        };
+      if (!parser.isOfType(part)) {
+        continue;
       }
+
+      const instance = createInstance(parser.name, part);
+
+      return {
+        ...acc,
+        ...instance.parse(acc),
+      };
     }
 
     throw new Error("Type not valid or not yet implemented.");
--- test/lessthan.test.ts
+++ test/lessthan.test.ts
@@ -3,21 +3,21 @@ import { Operators } from "../src/operators";
 
 test("it converts to to the correct value", () => {
   expect(parse("a lt string")).toStrictEqual({
-    a: { [Operators.LESS_THEN]: "string" },
+    a: { [Operators.LESS_THAN]: "string" },
   });
-  expect(parse("a lt 1")).toStrictEqual({ a: { [Operators.LESS_THEN]: 1 } });
-  expect(parse("a lt 0")).toStrictEqual({ a: { [Operators.LESS_THEN]: 0 } });
+  expect(parse("a lt 1")).toStrictEqual({ a: { [Operators.LESS_THAN]: 1 } });
+  expect(parse("a lt 0")).toStrictEqual({ a: { [Operators.LESS_THAN]: 0 } });
   expect(parse("a lt false")).toStrictEqual({
-    a: { [Operators.LESS_THEN]: false },
+    a: { [Operators.LESS_THAN]: false },
   });
   expect(parse("a lt true")).toStrictEqual({
-    a: { [Operators.LESS_THEN]: true },
+    a: { [Operators.LESS_THAN]: true },
   });
 });
 
 test("it parses multiple eq", () => {
   expect(parse("a lt 1 and b lt false")).toStrictEqual({
-    a: { [Operators.LESS_THEN]: 1 },
-    b: { [Operators.LESS_THEN]: false },
+    a: { [Operators.LESS_THAN]: 1 },
+    b: { [Operators.LESS_THAN]: false },
   });
 });
